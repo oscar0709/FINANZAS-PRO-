eac30541adb7b7681e1850263991650f9002fae4
oscar0709 <37028774+oscar0709@users.noreply.github.com>
2026-07-26 21:31:16 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,10 @@
 // Este es el único archivo que necesitas editar.
 
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDo5ixjxoZXwpfkAjyoUZB3i_I23NBoGVw",
+  authDomain: "finanzas-pro-4e80d.firebaseapp.com",
+  projectId: "finanzas-pro-4e80d",
+  storageBucket: "finanzas-pro-4e80d.firebasestorage.app",
+  messagingSenderId: "245265454766",
+  appId: "1:245265454766:web:1ad6542695e8f8caa47f3f"
 };
