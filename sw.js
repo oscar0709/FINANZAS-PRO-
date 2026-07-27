d7cef4c148eeba66cc1d46843156c8b318dfbcd6
oscar0709 <37028774+oscar0709@users.noreply.github.com>
2026-07-26 19:11:18 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "finanzas-pro-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE_NAME; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      if(cached) return cached;
+      return fetch(event.request).then(function(response){
+        return caches.open(CACHE_NAME).then(function(cache){
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(function(){
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
